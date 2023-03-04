088ceb6ebd692bebf2075548f8b12356aa2b7b54
crimson-ghost <brodiewatson95@gmail.com>
2023-03-04 08:59:38 +0000

    Create the blog app

---
+++ website/auth.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, url_for, redirect, request, flash
+from . import db 
+from .models import User
+from flask_login import login_user, logout_user, login_required
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    return render_template('login.html')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2') 
+
+        email_exists = User.query.filter_by(email=email).first() 
+        username_exists = User.query.filter_by(username=username).first()
+
+
+        if email_exists:
+            flash("Email already exists", category='error')
+        elif username_exists:
+            flash("Username is taken", category='error')
+        elif password1 != password2:
+            flash("Passwords don\t match", category='error')
+        elif len(username) < 2:
+            flash('Username too short', category='error')
+        elif len(password1) < 6:
+            flash('Password is too short', category='error')
+        elif len(email) < 4:
+            flash('Email is invalid', category='error')
+        else:
+            new_user = User(email=email, username=username, password=password1)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created', category='success')
+            redirect(url_for('views.home'))
+        
+
+    return render_template('signup.html')
+
+@auth.route('/logout')
+def logout():
+    return redirect(url_for('views.home'))
